689281dc320f5620340a865fa84906d3bb0b873e
David A. Ventimiglia <dventimi@gmail.com>
2017-04-03 11:45:06 -0700

    Finished the advanced solutions.

--- palin.erl
+++ palin.erl
@@ -1,6 +1,11 @@
 -module(palin).
 -export([palin/1,nopunct/1,palindrome/1]).
--export([server/1]).
+-export([
+	 server/0,
+	 server/1,
+	 proxy/1,
+	 proxy/2
+	]).
 
 % palindrome problem
 %
@@ -47,9 +52,13 @@ shunt([X|Xs],Ys) ->
 
 %% A palindrome checking server
 
+%% The basic solution creates a server process for a particular client
+%% Pid, and then waits for messages having the proper format,
+%% returning the appropriate response.
+
 server(Pid) ->
     receive
-	{check,Msg} ->
+	{check,Msg} ->				%Check for a palindrome.
 	    Pid ! {result,
 		   lists:flatten(
 		     io_lib:format(
@@ -60,6 +69,61 @@ server(Pid) ->
 			    false -> " is not "
 			end]))},
 	    server(Pid);
-	_ ->
+	_ ->					%Stop for ANY other message.
 	    ok
     end.
+
+%% The modified solution creates a server not tied to a particular
+%% client, and then waits for messages having the proper format (which
+%% now includes the client Pid), returning the appropriate response to
+%% the client.  Note also that now a diagnostic message is written to
+%% standard out indicating the client Pid.
+
+server() ->
+    receive
+	{check,Msg,Pid} ->			%Check for a palindrome.
+	    io:format("Server:  ~w~n",[self()]),
+	    Pid ! {result,
+		   lists:flatten(
+		     io_lib:format(
+		       "\"~s\"~sa palindrome.",
+		       [Msg,
+			case(palindrome(Msg)) of
+			    true -> " is ";
+			    false -> " is not "
+			end]))},
+	    server();
+	_ ->					%Stop for ANY other message.
+	    ok
+    end.
+
+    
+%% The replicating server solution creates a server that proxies
+%% messages to a list of servers in a "round-robin" fashion.  Note
+%% that the proxy server is agnostic about the server protocol.  I.e.,
+%% it just forwards messages to the servers, who must handle
+%% communication back to the client themselves.  Note also that a
+%% proxy server does not control the life-cycle of its proxied
+%% servers.  I.e., they must be started before calling proxy/1, with
+%% their Pids passed in in a list.  Likewise, the proxied servers must
+%% be stopped independent of the proxying server.  However, given that
+%% the proxy server is protocol-agnostic, if the proxied servers
+%% support stopping themselves in their own protocol, then the stop
+%% message CAN be sent through the proxy server, though whatever that
+%% message is, it must be sent multiple times once for ever server.
+%% In practice, it may be safer to stop the proxied servers directly.
+
+proxy(Servers) ->
+    proxy(Servers,Servers).
+
+proxy([],Servers) ->
+    proxy(Servers,Servers);
+proxy([X|Xs],Servers) ->
+    receive 
+	stop ->
+	    ok;
+	Msg ->
+	    X ! Msg,
+	    proxy(Xs,Servers)
+    end.
+
