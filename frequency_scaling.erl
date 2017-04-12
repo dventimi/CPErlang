0ae9ea5fac2b603a9c09aafd78755f0bffe6de94
David A. Ventimiglia <dventimi@gmail.com>
2017-04-12 15:50:11 -0700

    Got lifecycle mgmt to work sorta

--- frequency_scaling.erl
+++ frequency_scaling.erl
@@ -7,102 +7,112 @@
 %%   (c) Francesco Cesarini and Simon Thompson
 
 -module(frequency_scaling).
--export([start/0,allocate/0,deallocate/1,stop/0]).
--export([init/0]).
 -export([
-	 proxy/1,
-	 proxy/2
+	 allocate/0,
+	 deallocate/1,
+	 dump/0,
+	 backend_init/1,
+	 frontend_loop/1,
+	 frontend_loop/2,
+	 start/0,
+	 stop/0
 	]).
 -include_lib("eunit/include/eunit.hrl").
+
+start() ->
+    register(front,
+	     spawn(?MODULE,
+		   frontend_loop,
+		   [[
+		     spawn(?MODULE,backend_init,[[lists:seq(10,15)]]),
+		     spawn(?MODULE,backend_init,[[lists:seq(20,25)]])
+		    ]])).
+
+stop() ->
+    front ! {front,self(),stop}.
 
 %% These are the start functions used to create and
 %% initialize the server.
 
-start() ->
-    register(frequency,
-	     spawn(frequency, init, [])).
+frontend_loop(Backends) ->
+    frontend_loop(Backends,Backends).
 
-init() ->
-    Frequencies = {get_frequencies(), []},
-    loop(Frequencies).
-
-% Hard Coded
-get_frequencies() -> [10,11,12,13,14,15].
+frontend_loop([],Backends) ->
+    frontend_loop(Backends,Backends);
+frontend_loop([X|Xs],Backends) ->
+    receive 
+	{front,Pid,stop} ->
+	    lists:map(fun(Backend) ->
+    			      Backend ! {request,self(),stop}
+    		      end,
+    		      Backends),	    
+	    Pid ! {reply,ok};
+	{front,Pid,dump} ->
+	    Pid ! {reply,Backends},
+	    frontend_loop([X|Xs],Backends);
+	Msg ->
+	    X ! Msg,
+	    frontend_loop(Xs,Backends)
+    end.
 
 %% The Main Loop
 
-loop(Frequencies) ->
+backend_init(Free) ->
+    backend_loop({Free,[]}).
+
+backend_loop(Frequencies) ->
     receive
-	{request, Pid, allocate} ->
-	    {NewFrequencies, Reply} = allocate(Frequencies, Pid),
-	    Pid ! {reply, Reply},
-	    loop(NewFrequencies);
-	{request, Pid , {deallocate, Freq}} ->
-	    NewFrequencies = deallocate(Frequencies, Freq),
-	    Pid ! {reply, ok},
-	    loop(NewFrequencies);
-	{request, Pid, stop} ->
-	    Pid ! {reply, stopped}
+	{request,Pid,ping} ->
+	    Pid ! {reply,{pong,self()}},
+	    backend_loop(Frequencies);
+	{request,Pid,allocate} ->
+	    {NewFrequencies,Reply} = allocate(Frequencies,Pid),
+	    Pid ! {reply,Reply},
+	    backend_loop(NewFrequencies);
+	{request,Pid ,{deallocate,Freq}} ->
+	    NewFrequencies = deallocate(Frequencies,Freq),
+	    Pid ! {reply,ok},
+	    backend_loop(NewFrequencies);
+	{request,Pid,stop} ->
+	    Pid ! {reply,stopped}
     end.
 
 %% Functional interface
 
+dump() ->
+    front ! {front,self(),dump},
+    receive
+	{reply,Backends} ->
+	    io:format("~p~n",[Backends])
+    end.
+
 allocate() -> 
-    frequency ! {request, self(), allocate},
+    front ! {request,self(),allocate},
     receive 
-	{reply, Reply} -> Reply
+	{reply,Reply} -> Reply
     end.
 
 deallocate(Freq) -> 
-    frequency ! {request, self(), {deallocate, Freq}},
+    front ! {request,self(),{deallocate,Freq}},
     receive 
-	{reply, Reply} -> Reply
+	{reply,Reply} -> Reply
     end.
 
-stop() -> 
-    frequency ! {request, self(), stop},
-    receive 
-	{reply, Reply} -> Reply
-    end.
+%% stop() -> 
+%%     ?MODULE ! {request,self(),stop},
+%%     receive 
+%% 	{reply,Reply} -> Reply
+%%     end.
 
 
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
 
-allocate({[], Allocated}, _Pid) ->
-    {{[], Allocated}, {error, no_frequency}};
-allocate({[Freq|Free], Allocated}, Pid) ->
-    {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+allocate({[],Allocated},_Pid) ->
+    {{[],Allocated},{error,no_frequency}};
+allocate({[Freq|Free],Allocated},Pid) ->
+    {{Free,[{Freq,Pid}|Allocated]},{ok,Freq}}.
 
-deallocate({Free, Allocated}, Freq) ->
-    NewAllocated=lists:keydelete(Freq, 1, Allocated),
-    {[Freq|Free],  NewAllocated}.
-
-%% The replicating server solution creates a server that proxies
-%% messages to a list of servers in a "round-robin" fashion.  Note
-%% that the proxy server is agnostic about the server protocol.  I.e.,
-%% it just forwards messages to the servers, who must handle
-%% communication back to the client themselves.  Note also that a
-%% proxy server does not control the life-cycle of its proxied
-%% servers.  I.e., they must be started before calling proxy/1, with
-%% their Pids passed in in a list.  Likewise, the proxied servers must
-%% be stopped independent of the proxying server.  However, given that
-%% the proxy server is protocol-agnostic, if the proxied servers
-%% support stopping themselves in their own protocol, then the stop
-%% message CAN be sent through the proxy server, though whatever that
-%% message is, it must be sent multiple times once for ever server.
-%% In practice, it may be safer to stop the proxied servers directly.
-
-proxy(Servers) ->
-    proxy(Servers,Servers).
-
-proxy([],Servers) ->
-    proxy(Servers,Servers);
-proxy([X|Xs],Servers) ->
-    receive 
-	stop ->
-	    ok;
-	Msg ->
-	    X ! Msg,
-	    proxy(Xs,Servers)
-    end.
+deallocate({Free,Allocated},Freq) ->
+    NewAllocated=lists:keydelete(Freq,1,Allocated),
+    {[Freq|Free], NewAllocated}.
