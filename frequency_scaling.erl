b98183171be5c19dd9a77eae8f2b4082e2d16f51
David A. Ventimiglia <dventimi@gmail.com>
2017-04-12 11:42:30 -0700

    Starting out with the proxy server

--- frequency_scaling.erl
+++ frequency_scaling.erl
@@ -9,6 +9,10 @@
 -module(frequency_scaling).
 -export([start/0,allocate/0,deallocate/1,stop/0]).
 -export([init/0]).
+-export([
+	 proxy/1,
+	 proxy/2
+	]).
 -include_lib("eunit/include/eunit.hrl").
 
 %% These are the start functions used to create and
@@ -73,3 +77,32 @@ allocate({[Freq|Free], Allocated}, Pid) ->
 deallocate({Free, Allocated}, Freq) ->
     NewAllocated=lists:keydelete(Freq, 1, Allocated),
     {[Freq|Free],  NewAllocated}.
+
+%% The replicating server solution creates a server that proxies
+%% messages to a list of servers in a "round-robin" fashion.  Note
+%% that the proxy server is agnostic about the server protocol.  I.e.,
+%% it just forwards messages to the servers, who must handle
+%% communication back to the client themselves.  Note also that a
+%% proxy server does not control the life-cycle of its proxied
+%% servers.  I.e., they must be started before calling proxy/1, with
+%% their Pids passed in in a list.  Likewise, the proxied servers must
+%% be stopped independent of the proxying server.  However, given that
+%% the proxy server is protocol-agnostic, if the proxied servers
+%% support stopping themselves in their own protocol, then the stop
+%% message CAN be sent through the proxy server, though whatever that
+%% message is, it must be sent multiple times once for ever server.
+%% In practice, it may be safer to stop the proxied servers directly.
+
+proxy(Servers) ->
+    proxy(Servers,Servers).
+
+proxy([],Servers) ->
+    proxy(Servers,Servers);
+proxy([X|Xs],Servers) ->
+    receive 
+	stop ->
+	    ok;
+	Msg ->
+	    X ! Msg,
+	    proxy(Xs,Servers)
+    end.
