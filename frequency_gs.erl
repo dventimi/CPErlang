9a5368a03ade59ac8394a1bd71ad859bac5be91e
David A. Ventimiglia <dventimi@gmail.com>
2017-04-18 11:01:54 -0700

    Minor formatting changes

--- frequency_gs.erl
+++ frequency_gs.erl
@@ -6,7 +6,7 @@
 %%   http://www.erlangprogramming.org/
 %%   (c) Francesco Cesarini and Simon Thompson
 
--module(frequency).
+-module(frequency_gs).
 -export([start/0,allocate/0,deallocate/1,stop/0]).
 -export([init/0]).
 
@@ -18,8 +18,8 @@ start() ->
 	     spawn(frequency, init, [])).
 
 init() ->
-  Frequencies = {get_frequencies(), []},
-  loop(Frequencies).
+    Frequencies = {get_frequencies(), []},
+    loop(Frequencies).
 
 % Hard Coded
 get_frequencies() -> [10,11,12,13,14,15].
@@ -27,48 +27,47 @@ get_frequencies() -> [10,11,12,13,14,15].
 %% The Main Loop
 
 loop(Frequencies) ->
-  receive
-    {request, Pid, allocate} ->
-      {NewFrequencies, Reply} = allocate(Frequencies, Pid),
-      Pid ! {reply, Reply},
-      loop(NewFrequencies);
-    {request, Pid , {deallocate, Freq}} ->
-      NewFrequencies = deallocate(Frequencies, Freq),
-      Pid ! {reply, ok},
-      loop(NewFrequencies);
-    {request, Pid, stop} ->
-      Pid ! {reply, stopped}
-  end.
+    receive
+	{request, Pid, allocate} ->
+	    {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+	    Pid ! {reply, Reply},
+	    loop(NewFrequencies);
+	{request, Pid , {deallocate, Freq}} ->
+	    NewFrequencies = deallocate(Frequencies, Freq),
+	    Pid ! {reply, ok},
+	    loop(NewFrequencies);
+	{request, Pid, stop} ->
+	    Pid ! {reply, stopped}
+    end.
 
 %% Functional interface
 
 allocate() -> 
     frequency ! {request, self(), allocate},
     receive 
-	    {reply, Reply} -> Reply
+	{reply, Reply} -> Reply
     end.
 
 deallocate(Freq) -> 
     frequency ! {request, self(), {deallocate, Freq}},
     receive 
-	    {reply, Reply} -> Reply
+	{reply, Reply} -> Reply
     end.
 
 stop() -> 
     frequency ! {request, self(), stop},
     receive 
-	    {reply, Reply} -> Reply
+	{reply, Reply} -> Reply
     end.
-
 
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
 
 allocate({[], Allocated}, _Pid) ->
-  {{[], Allocated}, {error, no_frequency}};
+    {{[], Allocated}, {error, no_frequency}};
 allocate({[Freq|Free], Allocated}, Pid) ->
-  {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+    {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
 
 deallocate({Free, Allocated}, Freq) ->
-  NewAllocated=lists:keydelete(Freq, 1, Allocated),
-  {[Freq|Free],  NewAllocated}.
+    NewAllocated=lists:keydelete(Freq, 1, Allocated),
+    {[Freq|Free],  NewAllocated}.
